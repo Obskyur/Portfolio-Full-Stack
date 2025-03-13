83f57c4dbd997b53ac9f1243dba8bb1848c289ea
Obskyur <parkourman.miller@gmail.com>
2025-03-13 08:30:38 -0700

    feat: add navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import '@/styles/NavBar.css';
+
+
+export default function NavBar() {
+
+    return (
+        <header className="header">
+            <Link className="header__title-link" href="/">
+                <Image className="header__title-img" src="/avatar.png" alt="profile icon" width="100" height="100" ></Image>
+                <h1 className="header__title-text">Tristan Miller</h1>
+            </Link>
+            <nav className="header__nav">
+                <ul className="header__nav-list">
+                    <li className="header__nav-item"><Link className="header__nav-item-link" href="/">Home</Link></li>
+                    <li className="header__nav-item"><Link className="header__nav-item-link" href="/projects">Projects</Link></li>
+                    <li className="header__nav-item"><Link className="header__nav-item-link" href="/about">About</Link></li>
+                    <li className="header__nav-item"><Link className="header__nav-item-link" href="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
